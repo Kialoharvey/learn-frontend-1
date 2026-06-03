ded0dee3303492f3481885b3e128d4647357e536
Peter Kialo <kialoharvey@go.byuh.edu>
2026-06-02 18:28:46 -1000

    Created Home page under sources folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <section className="page">
+            <h2>Home</h2>
+            <p>Welcome to my PNG Culture and Tradition app.</p>
+        </section>
+    )
+}
+export default Home;
