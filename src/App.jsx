50fba1350d63eae8f08dd28398277e6d43f08802
Peter Kialo <kialoharvey@go.byuh.edu>
2026-06-02 18:50:41 -1000

    Changed App.jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,29 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
-function CultureCard({ title, description }) {
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </div>
-  );
-}
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [likes, setLikes] = useState(0);
-
   return (
-    <main className="app">
-      <h1>Learn Front End 3 React Practice</h1>
+    <BrowserRouter>
+      <main className="app">
+        <h1>Learn Front End 4</h1>
 
-      <p>
-        This is Week 2 leaning plan continued on React components, props, state, and JSX fundamentals.
-      </p>
-      <button onClick={() => setLikes(likes + 1)}>
-       Likes: {likes}
-       </button>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
 
-      <CultureCard
-        title="Papua New Guinea Culture & Tradition"
-        description="PNG is known for its language diversity, traditions and mountainous geography."
-      />
-
-      <CultureCard
-        title="React Component Practice"
-        description="This card is a reusable React component using props."
-      />
-      <CultureCard
-  title="Traditional PNG Dances"
-  description="Traditional dances are used during celebrations, ceremonies and cultural festivals."
-/>
-
-<CultureCard
-  title="PNG Food and Cooking"
-  description="Many PNG communities cook food using earth ovens and natural ingredients."
-/>
-
-      <section className="card">
-        <h2>State Practice</h2>
-        <p>Likes: {likes}</p>
-        <button onClick={() => setLikes(likes + 1)}>
-          Add Like
-        </button>
-      </section>
-    </main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
