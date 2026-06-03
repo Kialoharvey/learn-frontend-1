0b0f8975d393328861ba89756265bb8cbcb5909f
Peter Kialo <kialoharvey@go.byuh.edu>
2026-06-02 19:53:10 -1000

    Updated About info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,28 @@
-function About(){
-    return (
-        <section className="pages">
-            <h2>About</h2>
-            <p>This app is bulit purposely to practice React Router, forms, and hooks.</p>
-        </section>
-    )
+function About() {
+  return (
+    <section className="page">
+      <h2>About This Project</h2>
+
+      <p>
+        This project was created for CS 490 to practice frontend development
+        using React.
+      </p>
+
+      <p>
+        The goal is to learn React components, React Router, forms, hooks, and
+        API integration while building a website about Papua New Guinea culture.
+      </p>
+
+      <h3>Technologies Used</h3>
+
+      <ul>
+        <li>React</li>
+        <li>Vite</li>
+        <li>JavaScript</li>
+        <li>React Router</li>
+      </ul>
+    </section>
+  );
 }
+
 export default About;
