98d0f8399d71c2ea62f601ed6a003956f4956e4d
Peter Kialo <kialoharvey@go.byuh.edu>
2026-06-02 18:34:03 -1000

    Created About page under sources folder

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About(){
+    return (
+        <section className="pages">
+            <h2>About</h2>
+            <p>This app is bulit purposely to practice React Router, forms, and hooks.</p>
+        </section>
+    )
+}
+export default About;
