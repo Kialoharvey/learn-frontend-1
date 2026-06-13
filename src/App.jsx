593dad403171ab44d7490add6e5d9f1672d121a4
Peter Kialo <kialoharvey@go.byuh.edu>
2026-06-13 09:36:53 -1000

    updated my app.jsx to inlcude a signup login and post postsdetails form

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,35 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Posts from "./pages/Posts";
+import PostDetails from "./pages/PostDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <main className="app">
-        <h1>Learn Front End 4</h1>
+        <h1>PNG Culture Frontend</h1>
 
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/posts">Culture Posts</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
         </Routes>
       </main>
     </BrowserRouter>
