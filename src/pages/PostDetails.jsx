aa8b8ca30aa71193cd4aa839e4cf78830300c84e
Peter Kialo <kialoharvey@go.byuh.edu>
2026-06-13 10:07:54 -1000

    update on postsdetails.jsx

---
+++ src/pages/PostDetails.jsx
@@ -0,0 +1,10 @@
+function PostDetails() {
+  return (
+    <div>
+      <h1>Culture Post Details</h1>
+      <p>This page will show one culture post.</p>
+    </div>
+  );
+}
+
+export default PostDetails;
